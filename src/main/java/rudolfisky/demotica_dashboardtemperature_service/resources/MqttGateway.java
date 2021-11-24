07548767dd4457c79bd49d4e5d89ef733f7ae01d
Rudolfisky <33927733+Rudolfisky@users.noreply.github.com>
2021-11-24 12:00:07 +0100

    data deletion and mqtt connection with database

---
+++ src/main/java/rudolfisky/demotica_dashboardtemperature_service/resources/MqttGateway.java
@@ -0,0 +1,13 @@
+package rudolfisky.demotica_dashboardtemperature_service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+@MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
+public interface MqttGateway {
+
+
+    void senToMqtt(String data, @Header(MqttHeaders.TOPIC) String topic);
+}
