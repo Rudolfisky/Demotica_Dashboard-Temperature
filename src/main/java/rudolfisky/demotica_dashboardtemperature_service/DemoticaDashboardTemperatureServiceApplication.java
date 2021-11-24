e58e512c9dfb98aeaefd419d86ea7defce49df89
Rudolfisky <33927733+Rudolfisky@users.noreply.github.com>
2021-11-24 09:55:41 +0100

    initialization

---
+++ src/main/java/rudolfisky/demotica_dashboardtemperature_service/DemoticaDashboardTemperatureServiceApplication.java
@@ -0,0 +1,13 @@
+package rudolfisky.demotica_dashboardtemperature_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoticaDashboardTemperatureServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoticaDashboardTemperatureServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rudolfisky/demotica_dashboardtemperature_service/DemoticaDashboardTemperatureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rudolfisky.demotica_dashboardtemperature_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoticaDashboardTemperatureServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
