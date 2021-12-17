75dec37996dc4033810e980641e38dfbac262bdb
Rudolfisky <33927733+Rudolfisky@users.noreply.github.com>
2021-12-17 10:24:41 +0100

    failed test test

--- src/test/java/rudolfisky/demotica_dashboardtemperature_service/TempUnitTests.java
+++ src/test/java/rudolfisky/demotica_dashboardtemperature_service/TempUnitTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.util.Assert;
 import rudolfisky.demotica_dashboardtemperature_service.models.Temp;
 import rudolfisky.demotica_dashboardtemperature_service.resources.TempDB;
 import rudolfisky.demotica_dashboardtemperature_service.services.TempService;
@@ -60,5 +61,13 @@ public class TempUnitTests {
         //Assert
         verify(tempDB).deleteById(id);
     }
-
+    @Test
+    void failedTest() {
+        //Assign
+        boolean isTrue;
+        //Execute
+        isTrue = false;
+        //Assert
+        Assert.isTrue(isTrue, "is true");
+    }
 }
